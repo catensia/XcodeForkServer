6b9079a56086a822cd36ea4296aaf83a451e9f9e
catenary <catenarywkd1@gmail.com>
2021-04-29 19:58:26 +0900

    Initial Commit

---
+++ XcodeForkServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeForkServer
+//
+//  Created by 장수길 on 2021. 4. 29..
+//  Copyright © 2021년 장수길. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
